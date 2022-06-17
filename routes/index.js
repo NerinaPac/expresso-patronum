99fedcf4b2dced55c179c3a5c2aca3f3160f4c05
Nerina <paccner@gmail.com>
2022-06-16 21:52:04 -0300

    Buscador no terminado

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jeloouuuuu!' });
 });
 
+router.get('/buscar', (req, res)=>{
+  res.send('Estas en buscar');  
+  const { termino } = req.query;
+  console.log(termino);
+  //Magia de conexion a DB SELECT base de datos.
+});
+
 /* GET nosotros page (crear la ruta de nosotros) */
 router.get('/nosotros', (req, res)=>{
   res.render('pages/nosotros')
@@ -15,3 +22,10 @@ router.get('/contacto', (req, res) =>{
 res.render('pages/contacto')
 });
 module.exports = router;
+
+/* ejercitafcio = guardar en una variable lo que escribio el usuario en el campo y tirar un consolog */
+
+
+  
+  
+  
