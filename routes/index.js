bc9edb9e848a4657a7c5c4ee7dc5a24f0c0796c8
Nerina <paccner@gmail.com>
2022-06-16 21:23:33 -0300

    partials, routes y CSS

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jeloouuuuu!' });
 });
 
+/* GET nosotros page (crear la ruta de nosotros) */
+router.get('/nosotros', (req, res)=>{
+  res.render('pages/nosotros')
+});
+
+router.get('/contacto', (req, res) =>{
+res.render('pages/contacto')
+});
 module.exports = router;
