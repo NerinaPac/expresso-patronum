73c5e99e6e805b990f9446130c9b65702f766a88
Nerina <paccner@gmail.com>
2022-06-21 20:32:48 -0300

    metodo post

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,34 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jeloouuuuu!' });
 });
 
+/* GET agregar. */
+router.get('/agregar', (req, res) => {
+  res.render('pages/agregar');
+});
+
+/* 
+Manera de conseguir datos por metodo GET
+Params: (/:id) 
+req.query: (?termino=sarasa)
+
+Manera de conseguir datos por metodo POST
+req.body
+
+req se ultiza siempre porque es la SOLICITUD (de informacion)
+
+*/
+
+
+
+/* POST agregar-libro. */
+router.post('/agregar-libro', (req, res) => { /* Metodo post. Una realidad diferente al GET. Con el GET solo queremos navegar. */
+ 
+  //Aca vamos a conseguir lo que el usuario tipeo.
+  console.log(req.body); //Para levantar algo por metodo post (mostrar algo especifico en consolelog (trae un objeto)), tengo que usar req.body
+  let {titulo, autor, precio} = req.body;
+  res.send(`Agregaron ${titulo} ${autor} ${precio}`);
+});
+
 router.get('/buscar', (req, res)=>{
   res.send('Estas en buscar');  
   const { termino } = req.query;
