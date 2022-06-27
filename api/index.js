13a934ed554d483f0616cee4e8c5795c1755380f
Nerina <paccner@gmail.com>
2022-06-27 19:03:55 -0300

    Pagina Libros con utilizando findbypk.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-// Traemos la DB.
+// Traemos la DB
 const db = require('../models');
 
 // Esta mejor visto. Es una funcion exportada como una constante.
@@ -6,16 +6,19 @@ const db = require('../models');
 //se pone async para indicarle que los vamos ausar con aync away
 //include conecta la tabla del autor. JOIN .... autor
 const getBooks = async () => {
-    const libros = await db.libro.findAll({include: db.autor}).then(results => {
-        // Lo primero es lo que pasa en la base de datos, y lo otro devuelve los resultas en nodejs
-        //include hay que tener necesidad de ponerlo por alguna razon, porque va a sumar en el js, y va a afectar el performance
-
-        return results;
-
-    });
+    
+    const libros = await db.libro.findAll({
+                            include: db.autor
+                        })
+                        .then(results => {
+                            return results;
+                        });
 
     return libros;
 }
+
+ // Lo primero es lo que pasa en la base de datos, y lo otro devuelve los resultas en nodejs
+ //include hay que tener necesidad de ponerlo por alguna razon, porque va a sumar en el js, y va a afectar el performance
 
 const getAuthors = async () => {
     const authors = await db.autor.findAll()
@@ -26,7 +29,19 @@ const getAuthors = async () => {
     return authors;
 }
 
+const getBookById = async (id) => {
+    // console.log('Recibiste', id);
+    // Find by primary key / buscar por ID
+    const book = await db.libro.findByPk(id, {include: db.autor})
+                    .then(result => {
+                        return result;
+                    });
+
+    return book;
+};
+
 module.exports = {
     getBooks,
-    getAuthors
+    getAuthors,
+    getBookById
 }
--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,90 @@
 var express = require('express');
 var router = express.Router();
-//Inportamos nuestra api
+
+// Importamos nuestra api
 const api = require('../api');
 
 /* GET home page. */
 router.get('/', async (req, res) => {
-  // Llamamos a la funcion getBooks que esta dentro de api
+  // Llamamos a la función getBooks que está dentro de api
   const libros = await api.getBooks();
-  //console.log(libros);
-  res.render('index', { title: 'Jeloouuuuu!' });
-  //res.send(libros);
+  // console.log(libros);
+
+  // Si soy un servicio como Open weather map entonces siempre devuelto JSON usando res.send(resultado)
+  // angular
+  // res.send(libros);
+
+  // monorepo Vs monolito
+  // monorepo (un solo repositorio) fronter/backen. 
+  // monolito (malo): cuando un monorepo sale mal, se convierte en un monolito. Un repo que hace 55 cosas distintas.
+  res.render('index', { title: 'Ron Weasley', libros });
 });
 
+router.get('/libro/:id', async (req, res) => {
+  // params
+
+  // Le enviamos como parámetro el ID del libro que quiere ver el usuario
+  const libro = await api.getBookById(req.params.id);
+  // console.log(libro);
+
+  // res.send(`Estás viendo el libro ${req.params.id}`);
+  res.render('pages/libro', {libro});
+});
+
+// Crear una ruta de /autores
+// Mostrar el listado en JSON de todos los autores
 router.get('/autores', async (req, res) => {
   const autores = await api.getAuthors();
+
   res.send(autores);
 });
 
-// Si soy un servicio API como openweathermap, me sirve devolver un json
-// monorepo (un solo repositorio) fronter/backen. 
-// monolito (malo): cuando un monorepo sale mal, se convierte en un monolito. Un repo que hace 55 cosas distintas.
-//Esto es backen. Se puede hacer la front en otro framework
-
-//ejs es una herramieta de backer, no de fronten. Se queda muy limitada.
-
-/* GET agregar. */
+/* GET agregar */
 router.get('/agregar', (req, res) => {
   res.render('pages/agregar');
 });
 
-/* 
-Manera de conseguir datos por metodo GET
-Params: (/:id) 
-req.query: (?termino=sarasa)
-
-Manera de conseguir datos por metodo POST
-req.body
-
-req se ultiza siempre porque es la SOLICITUD (de informacion)
-
+/*
+  GET
+    req.params: (/:id)
+    req.query: (?termino=sarasa)
+  POST
+    req.body
 */
 
+/* POST agregar-libro */
+router.post('/agregar-libro', (req, res) => {
+  // Conseguir lo que el usuario tipeó
+  // Para levantar algo por método post tengo que usar
+  // req.body
+  console.log(req.body);
 
-
-/* POST agregar-libro. */
-router.post('/agregar-libro', (req, res) => { /* Metodo post. Una realidad diferente al GET. Con el GET solo queremos navegar. */
- 
-  //Aca vamos a conseguir lo que el usuario tipeo.
-  console.log(req.body); //Para levantar algo por metodo post (mostrar algo especifico en consolelog (trae un objeto)), tengo que usar req.body
   let {titulo, autor, precio} = req.body;
+
   res.send(`Agregaron ${titulo} ${autor} ${precio}`);
+  // res.render('pages/agregar');
 });
 
-router.get('/buscar', (req, res)=>{
-  res.send('Estas en buscar');  
-  const { termino } = req.query;
+/* GET nosotros page */
+router.get('/buscar', (req, res) => {
+  // query
+  // Guardar en una variable lo que escribió el usuario en el campo
+  let { termino } = req.query;
+  // let termino = req.query.termino;
   console.log(termino);
-  //Magia de conexion a DB SELECT base de datos.
+  // Magia de conexión a DB SELECT
+  // Mostrarlo en la terminal
+  res.send('Estas en buscar');
 });
 
-/* GET nosotros page (crear la ruta de nosotros) */
-router.get('/nosotros', (req, res)=>{
+/* GET nosotros page */
+router.get('/nosotros', (req, res) => {
   res.render('pages/nosotros')
 });
 
-router.get('/contacto', (req, res) =>{
-res.render('pages/contacto')
+/* GET contacto page */
+router.get('/contacto', (req, res) => {
+  res.render('pages/contacto')
 });
+
 module.exports = router;
